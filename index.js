c1f7f05bcf9b0a19ef878189c8dab404e17f1e2b
Abdulbaqi <adekomiabdulbaqi3@gmail.com>
2025-02-09 07:29:16 +0100

    starting html point

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 import express from 'express';
+import path from 'path';
+
+
+const __dirname = path.resolve();
 
 const app = express();
+const port = 3000;
+
+
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+
+app.listen(port, () => {
+    console.log(`Server is running on port http://localhost:${port}`);
+    });
