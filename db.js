6119695cec911dc3e619646588201fb1358975c8
Abdulbaqi <adekomiabdulbaqi3@gmail.com>
2025-02-12 23:48:16 +0100

    new server

--- db.js
+++ db.js
@@ -2,14 +2,9 @@ import mongoose from "mongoose";
 
 export default async function connectDB() {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
-            useUnifiedTopology: true,
-            useNewUrlParser: true,
-            useCreateIndex: true,
-        });
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
+        await mongoose.connect(process.env.MONGO_URI)
+        console.log("MongoDB Connected");
     } catch (error) {
-        console.error(`Error: ${error.message}`);
-        process.exit(1);
+        console.log(error);
     }
 }
--- server.js
+++ server.js
@@ -1,5 +1,22 @@
-import express from express;
-import connectDB from "./db";
-import path from "path";
+import express from 'express';
+import path from 'path';
+import connectDB from './db.js';
 
-connectDB()
+
+
+const __dirname = path.resolve();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+connectDB();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port http://localhost:${port}`);
+    });
